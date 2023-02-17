601f493519aab5bd5c8154bd372854b0e2afa498
Martin Blieninger <martinblieninger2208@gmail.com>
2023-02-17 23:17:52 +0100

    Version 3.0.6

--- src/js/patchInfo.js
+++ src/js/patchInfo.js
@@ -2,9 +2,9 @@ patchDateDay = 17;
 patchDateMonth = 02;
 
 patchTimeHour = 22;
-patchTimeMinute = 41;
+patchTimeMinute = 42;
 
 patchSizeF = 14;
-patchSizeS = 95;
+patchSizeS = 99;
 
 document.write("<b>-&nbsp;--&nbsp;Update&nbsp;Info&nbsp;--&nbsp-</b><br>Datum:&nbsp;" + patchDateDay + "&#46;" + patchDateMonth + "&#46;2023" + "<br>" + "Zeit&#58;&nbsp;" + patchTimeHour + "&#58;" + patchTimeMinute + "&nbsp;Uhr" + "<br>" + "Größe:&nbsp;" + patchSizeF + "&#46;" + patchSizeS + "&nbsp;MB");
--- src/js/version.js
+++ src/js/version.js
@@ -1,5 +1,5 @@
 MajorVersion = 3;
 SubVersion = 0;
-MinVersion = 5;
+MinVersion = 6;
 
 document.write("Version&nbsp;<small>crackyOS</small>&nbsp;" + MajorVersion + "&#46;" + SubVersion + "&#46;" + MinVersion);
